92403af20a059465952d639d83653a2e254cbfdc
Akash Prasad <akashprasadmk@gmail.com>
2023-12-06 13:27:10 +0530

    add at beginning and add at end functions are added

--- my_linked_list.c
+++ my_linked_list.c
@@ -9,46 +9,6 @@
     struct node *head;
 
 
-    int main(){
-        int choice = 0;
-        printf("\n------------WELCOME TO LINKED LIST OPERATION MENU------------\n");
-        while(choice != 8){
-            PrintOperationChoice();
-            scanf("%d",&choice);
-            switch(choice){
-                case 1 :
-                    AddBegin();
-                    break;
-                case 2 :
-                    AddEnd();
-                    break;
-                case 3 :
-                    AddAt();
-                    break;
-                case 4 :
-                    DeleteBegin();
-                    break;
-                case 5 :
-                    DeleteEnd();
-                    break;
-                case 6 :
-                    DeleteAt();
-                    break;
-                case 7 :
-                    Display();
-                    break;
-                case 8 :
-                    exit(0);
-                    break;
-                default:
-                    printf("Invalid Entry. Choose a valid option\n\n");
-                    
-            }
-
-        }
-        return 0;
-    }
-
     void PrintOperationChoice(){
         printf("\n\n-------------------------------------------\n");
         printf("             SELECT AN OPERATION               ");
@@ -79,5 +39,91 @@
             head = ptr;
             printf("Element added");
         }
+    }
+
+
+    void AddEnd(){
+        struct node *ptr = (struct node *)malloc(sizeof(struct node));
+        int item;
+        struct node *temp;
+
+        if(ptr == NULL){
+            printf("Overflow. Out of Memory");
+        }else{
+            printf("Enter the element to add : ");
+            scanf("%d",&item);
+            ptr->data = item;
+            if(head == NULL){
+                ptr->next = NULL;
+                head = ptr;
+            }else{
+                temp = head;
+                while(1){
+                    if(temp->next != NULL){
+                        temp = temp->next;
+                        continue;
+                    }
+                    temp->next = ptr;
+                    ptr->next = NULL;
+                    break;
+                }
+                printf("Element added");
+            }
+        }
+    }
+
+
+    void Display(){
+        struct node *temp;
+        temp = head;
+        while(1){
+            printf("%d\n",temp->data);
+            temp = temp->next;
+            if(temp->next == NULL){
+                printf("%d\n",temp->data);
+                break;
+            }
+        }
+    }
+
+
+    int main(){
+        int choice = 0;
+        printf("\n------------WELCOME TO LINKED LIST OPERATION MENU------------\n");
+        while(choice != 8){
+            PrintOperationChoice();
+            scanf("%d",&choice);
+            switch(choice){
+                case 1 :
+                    AddBegin();
+                    break;
+                case 2 :
+                    AddEnd();
+                    break;
+                // case 3 :
+                //     AddAt();
+                //     break;
+                // case 4 :
+                //     DeleteBegin();
+                //     break;
+                // case 5 :
+                //     DeleteEnd();
+                //     break;
+                // case 6 :
+                //     DeleteAt();
+                //     break;
+                case 7 :
+                    Display();
+                    break;
+                case 8 :
+                    exit(0);
+                    break;
+                default:
+                    printf("Invalid Entry. Choose a valid option.\n\n");
+                    
+            }
+
+        }
+        return 0;
     }
     
